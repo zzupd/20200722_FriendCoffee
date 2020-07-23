bf1e50f125978a6b9703b6131d161b54fd8d6f95
zzupd <zzupd@naver.com>
2020-07-23 15:49:37 +0900

    20200723_프랜드커피뷰페이지_header작업완료

---
+++ readMe_200723_View.txt
@@ -0,0 +1,7 @@
+﻿8 * x = 1200 - 56
+
+56 나머지는 143
+
+
+
+https://github.com/zzupd/20200722_FriendCoffee.git
--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 function goMarking() {
-
     alert("\"컨트롤키+D\" 를 눌러 즐겨찾기에 추가해주세요.");
+}
 
+function goModal() {    // 사용자정의 함수명
+    document.getElementById("modalArea").style.display="block";
+}
+
+function popUpClose() {
+    document.getElementById("modalArea").style.display="none";
 }
